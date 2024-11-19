c3b3266038bc79aea87252d6ea2f39e738922917
Mateo Llerena <mateollerena40@gmail.com>
2024-11-18 22:40:06 -0500

    Corrigiendo Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rick_and_morty_app/config/config.dart';
+
+void main(){
+  runApp(const ProviderScope(child: MyApp()));
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
